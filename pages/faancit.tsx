9cee706e55943ab175ecc8508f3e38366ad674dc
Wing Wong <me@nikowong.com>
2022-10-11 22:40:05 +0100

    remove darkmode support

--- pages/faancit.tsx
+++ pages/faancit.tsx
@@ -151,6 +151,15 @@ const Faancit: NextPage = () => {
   const upperChar: JyutpingChar | null = outputChars ? outputChars[1] : null;
   const lowerChar: JyutpingChar | null = outputChars ? outputChars[2] : null;
 
+  const CharDetails = ({ char }: any) => (
+    <>
+      <span className='text-blue-500 '>{char.sing}</span>
+      <span className='text-green-500'>{char.wan}</span>
+      <span className=''>{char.tone}</span>
+      <span className='ml-5'>{Jyutping.getSing(char)}聲</span>
+    </>
+  );
+
   return (
     <div className='container mx-auto text-left max-w-sm'>
       <Head>
@@ -160,6 +169,7 @@ const Faancit: NextPage = () => {
       <h1 className='my-5 text-xl text-center'>粵語反切轉換</h1>
       <div className='input my-3'>
         <input
+          className='border-solid border-2 rounded-md'
           type='text'
           id='toFaancit'
           placeholder='輸入反切字'
@@ -171,36 +181,15 @@ const Faancit: NextPage = () => {
       <div className='output flex flex-col justify-between h-28'>
         <p>
           上字：
-          {upperChar && (
-            <>
-              <span className='text-blue-500'>{upperChar.sing}</span>
-              <span className='text-blue-500'>{upperChar.wan}</span>
-              <span className=''>{upperChar.tone}</span>
-              <span className='ml-5'>{Jyutping.getSing(upperChar)}聲</span>
-            </>
-          )}
+          {upperChar && <CharDetails char={upperChar} />}
         </p>
         <p>
           下字：
-          {lowerChar && (
-            <>
-              <span className='text-green-500'>{lowerChar.sing}</span>
-              <span className='text-green-500'>{lowerChar.wan}</span>
-              <span className=''>{lowerChar.tone}</span>
-              <span className='ml-5'>{Jyutping.getSing(lowerChar)}聲</span>
-            </>
-          )}
+          {lowerChar && <CharDetails char={lowerChar} />}
         </p>
         <p>
           反切讀音：
-          {outputChar && (
-            <>
-              <span className='text-blue-500'>{outputChar.sing}</span>
-              <span className='text-green-500'>{outputChar.wan}</span>
-              <span className=''>{outputChar.tone}</span>
-              <span className='ml-5'>{Jyutping.getSing(outputChar)}聲</span>
-            </>
-          )}
+          {outputChar && <CharDetails char={outputChar} />}
         </p>
       </div>
     </div>
