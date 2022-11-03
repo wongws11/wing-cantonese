806cd1e531130ce73afd41d94290a4b490ac96cc
Wing Wong <me@nikowong.com>
2022-11-03 21:31:48 +0000

    clean up fanncit page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return <div className="container mx-auto text-center">
+		<h1>This Web Application is still a work in progress.</h1>
+	</div>;
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
     <>
       <nav className='flex items-center flex-wrap bg-gray-900 p-3 '>
         <Link className='inline-flex items-center p-2 mr-4'  href='/'>
-            <span className='text-xl text-white font-bold uppercase tracking-wide'>Navbar</span>
+            <span className='text-xl text-white font-bold uppercase tracking-wide'>Wing's Cantonese</span>
         </Link>
         <Link className='inline-flex items-center p-2 mr-4' href='/faancit'>
             <span className='text-xl text-white font-bold uppercase tracking-wide'>反切</span>
