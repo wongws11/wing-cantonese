875cecd3564ba1c2616de0039541584ed9f075be
Wing <me@nikowong.com>
2024-03-30 12:03:57 +0000

    no need front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Page() {
-	return (
-		<div className='container mx-auto text-center'>
-			<h1>This Web Application is still a work in progress.</h1>
-		</div>
-	);
+  redirect("/faancit");
 }
