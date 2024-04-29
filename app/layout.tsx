3c768407b91788c3aad3cceb9bdfc0b217b0ccce
Wing <me@nikowong.com>
2024-04-29 12:09:48 +0100

    google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,26 @@
-import { Navbar } from '../components/Navbar';
-import './global.scss';
+import { Navbar } from "../components/Navbar";
+import Script from "next/script";
+import "./global.scss";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<html>
-			<head>
-				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
-			</head>
-			<body>
-				<Navbar />
-				{children}
-			</body>
-		</html>
-	);
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-2MYL11Y6QW"
+      />
+      <Script src="./tag.js" />
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
 }
